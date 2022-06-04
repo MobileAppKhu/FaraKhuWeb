aa6f81407cd173d973fb0b4cad2a1601e6d878ad
Arsham79 <arsham.ramezani@gmail.com>
2022-06-04 18:50:02 +0430

    fix: theme provider probelm fixed

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,14 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 import { getTranslate } from '../../localization'
 import useStyles from './styles/index.style'
 
 const Header = () => {
   const classes = useStyles()
-  return <div className={classes.root}>{getTranslate('ارتباط با ما')}</div>
+  return (
+    <div className={classes.root}>
+      <Typography variant="h3">{getTranslate('ارتباط با ما')}</Typography>
+    </div>
+  )
 }
 export default Header
