8f01f1dec2e05bde742f2058cb6039f65a03a654
Hasan Mir <seyed.hasan.mir@gmail.com>
2022-07-24 20:57:10 +0430

    make Profile page responsive

--- src/pages/profile/index.style.ts
+++ src/pages/profile/index.style.ts
@@ -2,21 +2,35 @@ import { makeStyles } from '@mui/styles'
 import bgImg from '../../assets/images/khu_env_min.jpg'
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    color: '#000',
+  outerContainer: {
     background: `#eee url("${bgImg}") no-repeat fixed`,
     backgroundSize: 'cover',
     minHeight: '100vh',
+  },
+  innerContainer: {
+    color: '#000',
     display: 'flex',
+    minHeight: '100vh',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: '5rem 0',
   },
   infoCardContainer: {
     display: 'flex',
     position: 'relative',
     flexDirection: 'column',
     width: '60%',
+    [theme.breakpoints.down(1200)]: {
+      width: '70%',
+    },
+    [theme.breakpoints.down(1000)]: {
+      width: '80%',
+    },
+    [theme.breakpoints.down(840)]: {
+      width: '85%',
+    },
     borderTopLeftRadius: 10,
+    transition: 'all 0.3s',
 
     '& .editButton': {
       position: 'absolute',
@@ -44,50 +58,73 @@ const useStyles = makeStyles((theme) => ({
       borderBottomLeftRadius: 20,
       color: theme.palette.primary.light,
       transform: 'rotate(-90deg)',
+      transition: 'all 0.3s',
+
+      [theme.breakpoints.down(450)]: {
+        left: -25,
+        padding: '12px 20px',
+        fontWeight: 200,
+        fontSize: 18,
+      },
     },
 
     '& .top': {
       display: 'flex',
       borderTopLeftRadius: 10,
+      [theme.breakpoints.down(740)]: {
+        flexDirection: 'column',
+      },
 
       '& .right': {
         display: 'flex',
+        [theme.breakpoints.down(740)]: {
+          display: 'block',
+        },
         flex: 2.5,
         background: theme.palette.primary.light,
         boxShadow: '4px 7px 12px 0 rgb(0 0 0 / 33%)',
-      },
 
-      '& .rightContent': {
-        background: '#f1f0f4',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        padding: '30px 20px',
-        borderTopLeftRadius: 35,
-
-        '& .imageName': {
+        '& .rightContent': {
+          background: '#f1f0f4',
           display: 'flex',
           flexDirection: 'column',
-          '& img': {
-            objectFit: 'cover',
-            width: 200,
-            height: 200,
-            borderRadius: 15,
+          alignItems: 'center',
+          padding: '30px 20px',
+          borderTopLeftRadius: 35,
+          [theme.breakpoints.down(740)]: {
+            borderTopLeftRadius: 0,
           },
-          '& .name': {
-            textAlign: 'center',
-            fontWeight: 500,
-            marginTop: 15,
-            color: theme.palette.primary.onContainer,
-          },
-        },
 
-        '& .aboutMe': {
-          fontWeight: 'normal',
-          textAlign: 'center',
-          fontSize: 14,
-          color: theme.palette.primary.onContainer,
-          marginTop: 35,
+          '& .imageName': {
+            display: 'flex',
+            flexDirection: 'column',
+            '& img': {
+              objectFit: 'cover',
+              width: 200,
+              height: 200,
+              [theme.breakpoints.down(400)]: {
+                width: 160,
+                height: 160,
+              },
+
+              borderRadius: 15,
+              transition: 'all 0.3s',
+            },
+            '& .name': {
+              textAlign: 'center',
+              fontWeight: 500,
+              marginTop: 15,
+              color: theme.palette.primary.onContainer,
+            },
+          },
+
+          '& .aboutMe': {
+            fontWeight: 'normal',
+            textAlign: 'center',
+            fontSize: 14,
+            color: theme.palette.primary.onContainer,
+            marginTop: 35,
+          },
         },
       },
 
@@ -99,6 +136,13 @@ const useStyles = makeStyles((theme) => ({
         padding: 40,
         borderTopLeftRadius: 10,
         borderBottomLeftRadius: 5,
+        [theme.breakpoints.down(740)]: {
+          borderTopLeftRadius: 0,
+        },
+        [theme.breakpoints.down(450)]: {
+          borderBottomLeftRadius: 0,
+          boxShadow: '4px 7px 12px 0 rgb(0 0 0 / 33%)',
+        },
 
         '& .emailId': {
           display: 'flex',
@@ -108,7 +152,15 @@ const useStyles = makeStyles((theme) => ({
           '& .item': {
             display: 'flex',
             alignItems: 'center',
-            columnGap: 25,
+            columnGap: 10,
+
+            '& .email': {
+              fontSize: 18,
+              [theme.breakpoints.down(900)]: {
+                fontSize: 16,
+              },
+              transition: 'font-size 0.3s',
+            },
           },
         },
 
@@ -125,6 +177,10 @@ const useStyles = makeStyles((theme) => ({
           '& ul': {
             display: 'flex',
             flexWrap: 'wrap',
+            [theme.breakpoints.down(450)]: {
+              flexWrap: 'initial',
+              flexDirection: 'column',
+            },
             rowGap: 20,
             columnGap: 30,
             padding: 0,
@@ -136,7 +192,18 @@ const useStyles = makeStyles((theme) => ({
               alignItems: 'center',
               columnGap: 10,
               width: '40%',
-              fontWeight: 500,
+              [theme.breakpoints.down(450)]: {
+                width: '100%',
+              },
+
+              '& .itemName': {
+                fontWeight: 500,
+                color: '#000',
+                transition: 'all 0.3s',
+                [theme.breakpoints.down(450)]: {
+                  fontSize: 14,
+                },
+              },
 
               '& .itemBullet': {
                 display: 'block',
@@ -154,31 +221,74 @@ const useStyles = makeStyles((theme) => ({
     '& .bottom': {
       display: 'flex',
       height: 80,
+      [theme.breakpoints.down(450)]: {
+        flexDirection: 'column',
+        height: 'initial',
+      },
 
       '& .socialMedia': {
         width: '91%',
+        [theme.breakpoints.down(1285)]: {
+          width: '88%',
+        },
+        [theme.breakpoints.down(740)]: {
+          width: '85%',
+        },
+        [theme.breakpoints.down(600)]: {
+          width: '80%',
+        },
+        [theme.breakpoints.down(500)]: {
+          width: '75%',
+        },
+        [theme.breakpoints.down(450)]: {
+          width: '100%',
+          height: 60,
+          boxShadow: '4px 7px 12px 0 rgb(0 0 0 / 33%)',
+        },
         background: theme.palette.primary.main,
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         columnGap: 50,
+        [theme.breakpoints.down(600)]: {
+          columnGap: 30,
+        },
         boxShadow: '4px 5px 12px 0 rgb(0 0 0 / 31%)',
+        transition: 'all 0.3s',
 
         '& .icon': {
           color: theme.palette.primary.light,
           fontSize: 50,
+          [theme.breakpoints.down(600)]: {
+            fontSize: 40,
+          },
+          transition: 'all 0.3s',
         },
       },
 
       '& .exitButtonContainer': {
         width: '9%',
+        [theme.breakpoints.down(1285)]: {
+          width: '12%',
+        },
+        [theme.breakpoints.down(740)]: {
+          width: '15%',
+        },
+        [theme.breakpoints.down(600)]: {
+          width: '20%',
+        },
+        [theme.breakpoints.down(500)]: {
+          width: '25%',
+        },
         display: 'flex',
+        [theme.breakpoints.down(450)]: {
+          display: 'none',
+        },
         alignItems: 'center',
         justifyContent: 'center',
-
         borderTopRightRadius: 20,
-
         position: 'relative',
+        transition: 'width 0.3s',
 
         '& .exitButton': {
           width: '90%',
@@ -198,6 +308,35 @@ const useStyles = makeStyles((theme) => ({
         '& .icon': {
           color: theme.palette.primary.main,
           fontSize: 40,
+        },
+      },
+
+      '& .fullWidthExitButton': {
+        display: 'none',
+        [theme.breakpoints.down(450)]: {
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          background: '#fff',
+          border: `1px solid ${theme.palette.primary.main}`,
+          boxShadow:
+            '4px 0px 12px 0 rgb(0 0 0 / 33%), 0px 2px 5px 0 rgb(0 0 0 / 30%)',
+        },
+        height: 60,
+        padding: 10,
+        columnGap: 5,
+        borderRadius: 0,
+        transition: 'width 0.3s',
+
+        '& span': {
+          color: theme.palette.primary.main,
+          fontWeight: 500,
+          fontSize: 25,
+        },
+
+        '& .icon': {
+          color: theme.palette.primary.main,
+          fontSize: 35,
         },
       },
     },
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { IconButton, Typography } from '@mui/material'
+import { Button, Container, IconButton, Typography } from '@mui/material'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import GoogleIcon from '@mui/icons-material/Google'
 import LogoutIcon from '@mui/icons-material/Logout'
@@ -19,6 +19,9 @@ const Profile = () => {
 
   // temp
   const role = 'دانشجو'
+  const name = 'امیرحسین هدایتی'
+  const id = '982023031'
+  const email = 'hedayati@khu.ac.ir'
   const favorites = [
     'یادگیری ماشین',
     'محاسبات ابری',
@@ -31,82 +34,103 @@ const Profile = () => {
   ]
 
   return (
-    <div className={classes.container}>
-      <div className={classes.infoCardContainer}>
-        <div className="editButton">
-          <IconButton>
-            <EditIcon fontSize="large" color="primary" />
-          </IconButton>
-        </div>
-        <Typography variant="h3" className="role">
-          {role}
-        </Typography>
-        <div className="top">
-          <div className="right">
-            <div className="rightContent">
-              <div className="imageName">
-                <img src={avatar} alt="تصویر پروفایل کاربر" />
-                <Typography variant="h3" className="name">
-                  امیرحسین هدایتی
-                </Typography>
-              </div>
-              <Typography variant="body1" className="aboutMe">
-                طراح UI/UX طراح UI/UX طراح UI/UX طراح UI/UX طراح UI/UX طراح
-                UI/UX
-              </Typography>
-            </div>
-          </div>
-          <div className="left">
-            <div className="emailId">
-              <div className="item">
-                <Typography variant="h3">
-                  {`${getTranslate('شماره دانشجویی')}:`}
-                </Typography>
-                <Typography variant="h3" color="primary.onContainer">
-                  982023031
-                </Typography>
-              </div>
-              <div className="item">
-                <Typography variant="h3">
-                  {`${getTranslate('ایمیل')}:`}
-                </Typography>
-                <Typography variant="h3">hedayati@khu.ac.ir</Typography>
-              </div>
-            </div>
-            <div className="separator" />
-            <div className="favorites">
-              <Typography variant="h3" color="primary.main">
-                {getTranslate('علاقه‌مندی‌ها:')}
-              </Typography>
-              <ul>
-                {favorites.map((favorite) => (
-                  <li>
-                    <span className="itemBullet" />
-                    {favorite}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="bottom">
-          <div className="socialMedia">
-            <IconButton>
-              <LinkedInIcon className="icon" />
-            </IconButton>
-            <IconButton>
-              <GoogleIcon className="icon" />
-            </IconButton>
-          </div>
-          <div className="exitButtonContainer">
-            <div className="exitButton">
-              <IconButton>
-                <LogoutIcon className="icon" />
+    <div className={classes.outerContainer}>
+      <Container maxWidth="xl" sx={{ p: 0 }}>
+        <div className={classes.innerContainer}>
+          <div className={classes.infoCardContainer}>
+            <div className="editButton">
+              <IconButton title={getTranslate('ویرایش')}>
+                <EditIcon fontSize="large" color="primary" />
               </IconButton>
             </div>
+            <Typography variant="h3" className="role">
+              {role}
+            </Typography>
+            <div className="top">
+              <div className="right">
+                <div className="rightContent">
+                  <div className="imageName">
+                    <img src={avatar} alt="تصویر پروفایل کاربر" />
+                    <Typography variant="h3" className="name">
+                      {name}
+                    </Typography>
+                  </div>
+                  <Typography variant="body1" className="aboutMe">
+                    طراح UI/UX طراح UI/UX طراح UI/UX طراح UI/UX طراح UI/UX طراح
+                    UI/UX
+                  </Typography>
+                </div>
+              </div>
+              <div className="left">
+                <div className="emailId">
+                  <div className="item">
+                    <Typography variant="h3">
+                      {`${getTranslate('شماره دانشجویی')}:`}
+                    </Typography>
+                    <Typography variant="h3" color="primary.onContainer">
+                      {Number(id).toLocaleString('fa-IR', {
+                        useGrouping: false,
+                      })}
+                    </Typography>
+                  </div>
+                  <div className="item">
+                    <Typography variant="h3">
+                      {`${getTranslate('ایمیل')}:`}
+                    </Typography>
+                    <Typography variant="h3" className="email">
+                      {email}
+                    </Typography>
+                  </div>
+                </div>
+                <div className="separator" />
+                <div className="favorites">
+                  <Typography variant="h3" color="primary.main">
+                    {getTranslate('علاقه‌مندی‌ها:')}
+                  </Typography>
+                  <ul>
+                    {favorites.map((favorite) => (
+                      <li>
+                        <span className="itemBullet" />
+                        <Typography
+                          variant="body1"
+                          className="itemName"
+                          component="span"
+                        >
+                          {favorite}
+                        </Typography>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="bottom">
+              <div className="socialMedia">
+                <IconButton>
+                  <LinkedInIcon className="icon" />
+                </IconButton>
+                <IconButton>
+                  <GoogleIcon className="icon" />
+                </IconButton>
+              </div>
+              <div className="exitButtonContainer">
+                <div className="exitButton">
+                  <IconButton title={getTranslate('خروج از حساب کاربری')}>
+                    <LogoutIcon className="icon" />
+                  </IconButton>
+                </div>
+              </div>
+              <Button
+                className="fullWidthExitButton"
+                title={getTranslate('خروج از حساب کاربری')}
+              >
+                <LogoutIcon className="icon" />
+                <Typography component="span">{getTranslate('خروج')}</Typography>
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      </Container>
     </div>
   )
 }
